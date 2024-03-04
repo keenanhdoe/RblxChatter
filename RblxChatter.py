10b72f1921bfb444148fb8ccd979e6db1ef9eaef
keenanhdoe <161536510+keenanhdoe@users.noreply.github.com>
2024-03-04 09:13:46 -0600

    Minimized RblxChatter.py

--- RblxChatter.py
+++ RblxChatter.py
@@ -1,18 +1,17 @@
-version = open('verid', 'r')
-ver = version.readline()
-ver = ver.strip()
-verlist = ver.split('.')
-version.close()
+with open('verid', 'r') as version:
+    ver = version.readline().strip()
+    verlist = ver.split('.')
+
 print('-' * 10, f'RblxChatter v{ver}', '-' * 10)
+
 from bs4 import BeautifulSoup
 import requests
 import debugger
 
-
 debugger.info('Checking for update...')
 url = 'https://raw.githubusercontent.com/sc1l/RblxChatter/main/verid'
-
 response = requests.get(url)
+
 if response.status_code == 200:
     rep = response.text
     soup = BeautifulSoup(rep, 'html.parser')
@@ -21,50 +20,42 @@ if response.status_code == 200:
     debugger.debug(f'local = {ver, verlist}, online = {clean_text, onlist}')
     debugger.info(f'Checked online version: {clean_text}')
     debugger.info(f'Checked local version: {ver}')
+
     if verlist == onlist:
-        debugger.info(f'this version is lastest version.')
-    elif verlist[0] < onlist[0]:
-        debugger.warn(f'outdated version! please update to {clean_text}')
-    elif verlist[0] == onlist[0] and verlist[1] < onlist[1]:
-        debugger.warn(f'outdated version! please update to {clean_text}')
-    elif verlist[0] == onlist[0] and verlist[1] == onlist[1] and verlist[2] < onlist[2]:
-        debugger.warn(f'outdated version! please update to {clean_text}')
-else : 
-    debugger.warn('failed to check for update!')
+        debugger.info('This version is the latest version.')
+    elif verlist < onlist:
+        debugger.warn(f'Outdated version! Please update to {clean_text}')
 
-
+else:
+    debugger.warn('Failed to check for an update!')
 
 try:
-    
-    import random
-    debugger.debug('Imported random')
-    import pyautogui
-    debugger.debug('Imported pyautogui')
-    import time
-    debugger.debug('Imported time')
+    import random, pyautogui, time
     from pywinauto.keyboard import send_keys
-    debugger.debug('Imported pywinauto...')
+    debugger.debug('Imported random, pyautogui, time, pywinauto...')
     debugger.sinput('Please select your .tpl file(Full name)')
-    tpl = open(input('>>> '), "r")
-    text = tpl.read()
-    message = text.split("\n")
-    tpl.close()
-    min = debugger.input('Please select your min second(default 9)')
-    max = debugger.input('Please select your max second(default 15)')
+    
+    with open(input('>>> '), "r") as tpl:
+        message = tpl.read().split("\n")
+
+    min_sec = debugger.input('Please select your min second(default 9)')
+    max_sec = debugger.input('Please select your max second(default 15)')
+    
     debugger.info('Waiting for 5 seconds')
     time.sleep(5)
+
     while True:
         pyautogui.press('/')
-        debugger.info('opened chat')
+        debugger.info('Opened chat')
         time.sleep(0.5)
         msg = random.choice(message)
         pyautogui.write(msg)
-        debugger.info(f'chatted {msg}')
+        debugger.info(f'Chatted {msg}')
         time.sleep(1)
         send_keys('{ENTER}')
         send_keys('{ENTER}')
-        debugger.info('pressed Enter')
-        wait = random.randint(min, max)
+        debugger.info('Pressed Enter')
+        wait = random.randint(int(min_sec), int(max_sec))
         debugger.info(f'Waiting for {wait} seconds...')
         time.sleep(wait)
 
